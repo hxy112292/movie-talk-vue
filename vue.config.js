f0b2687044162fc58252141c348fe5aa237915bd
983771687@qq.com <github!112292>
2020-10-05 18:40:34 +0800

    modify api url

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://www.hd-wallet.com:8000',
         ws: true,
         changeOrigin: true
       }
